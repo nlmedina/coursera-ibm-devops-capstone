37d8ba2bad072166b863e216aa1b23181122f0fd
Nico Medina <nico@nlmedina.com>
2025-10-18 12:42:53 -0700

    add test_method_not_allowed to tests/test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,3 +135,8 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
